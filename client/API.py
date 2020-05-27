2b0ac889543fa83781d29718b6af928fd8c8b1fc
Robin Faber <r.j.faber@student.tudelft.nl>
2020-05-27 12:42:01 +0200

    Implement the setting up of a new machine Implement running commands through SSH

--- client/API.py
+++ client/API.py
@@ -1,35 +1,37 @@
 import boto3
 import time
 import socket
+import paramiko
 
 
 def fit_predict(classifier, train_x, train_y, test_x):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1', 8080))
-    client.send(b'get_instance')
-    instance_id = client.recv(4096).decode("utf-8")
-    print(instance_id)
+    master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    key = paramiko.RSAKey.from_private_key_file("CC.pem")
+    instance_ip = get_instance(master)
 
-    ssm_client = boto3.client('ssm')
-    response = ssm_client.send_command(
-        InstanceIds=[instance_id],
-        DocumentName="AWS-RunShellScript",
-        Parameters={
-            'commands': [
-                'echo fit and predict'
-            ]
-        }
-    )
-    time.sleep(1)
-    command_id = response['Command']['CommandId']
-    output = ssm_client.get_command_invocation(
-        CommandId=command_id,
-        InstanceId=instance_id,
-    )
-    print(output['StandardOutputContent'])
+    instance_ssh = paramiko.SSHClient()
+    instance_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    instance_ssh.connect(hostname=instance_ip, username="ubuntu", pkey=key)
 
+    stdin, stdout, stderr = instance_ssh.exec_command("rm test.py;"
+                                                      "printf \"def main():\n\tprint(\\\"hello world\\\")\nmain()\" >>test.py;"
+                                                      "python3 test.py")
+    stdout.channel.recv_exit_status()
+    print(stdout.read().decode("utf-8"))
+
+    free_instance(instance_ip, master)
+
+
+def get_instance(master):
+    master.connect(('127.0.0.1', 8080))
+    master.send(b'get_instance')
+    instance_ip = master.recv(4096).decode("utf-8")
+    return instance_ip
+
+
+def free_instance(instance_ip, master):
     print("Releasing instance")
-    free = f"free_{instance_id}".encode("utf-8")
-    client.send(free)
+    free = f"free_{instance_ip}".encode("utf-8")
+    master.send(free)
+    master.close()
     print("Client closed")
-
--- client/Master.py
+++ client/Master.py
@@ -1,6 +1,7 @@
 import boto3
 import socket
 import time
+import paramiko
 
 
 class Master:
@@ -9,16 +10,18 @@ class Master:
     ec2_client = boto3.client('ec2')
 
     def __init__(self):
+        self.key = paramiko.RSAKey.from_private_key_file("CC.pem")
         running_instances = list(self.ec2.instances.filter(
             Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]))
-        self.available_instances = list(map(lambda x: x.id, running_instances))
+        self.available_instances = list(map(lambda x: x.public_ip_address, running_instances))
+        #self.available_instances = []
 
     def get_instance(self):
         if len(self.available_instances) == 0:
             return self.start_instance()
-        instance_id = self.available_instances[0]
-        self.available_instances.remove(instance_id)
-        return instance_id
+        instance_ip = self.available_instances[0]
+        self.available_instances.remove(instance_ip)
+        return instance_ip
 
     def start_instance(self):
         stopped_instances = list(self.ec2.instances.filter(
@@ -30,7 +33,7 @@ class Master:
         new_instance = stopped_instances[0]
         self.ec2_client.start_instances(InstanceIds=[new_instance.id], DryRun=False)
         new_instance.wait_until_running()
-        return new_instance.id
+        return new_instance.public_ip_address
 
     def create_instance(self):
         new_instance = self.ec2.create_instances(ImageId=self.IMAGE_ID, InstanceType='t2.micro', MinCount=1, MaxCount=1,
@@ -38,13 +41,37 @@ class Master:
                     IamInstanceProfile={'Name': 'SSM'})
         new_instance[0].wait_until_running()
         time.sleep(60)
-        return new_instance[0].id
+        new_instance[0].reload()
+        self.setup_instance(new_instance[0].public_ip_address)
+        return new_instance[0].public_ip_address
 
-    def stop_instance(self, instance_id):
+    def stop_instance(self, instance_ip):
+        filters = [{
+            'Name': 'ip-address',
+            'Values': [instance_ip],
+        }]
+        results = self.ec2_client.describe_instances(Filters=filters)
+        instance_id = results['Reservations'][0]['Instances'][0]['InstanceId']
+        self.ec2_client.stop_instances(InstanceIds=[instance_id], DryRun=False)
         return
 
-    def free_instance(self, instance_id):
-        self.available_instances.append(instance_id)
+    def free_instance(self, instance_ip):
+        self.available_instances.append(instance_ip)
+
+    def setup_instance(self, instance_ip):
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        print("Setting up instance")
+        client.connect(hostname=instance_ip, username="ubuntu", pkey=self.key)
+        stdin, stdout, stderr = client.exec_command("sudo apt install -y python3-pip")
+        stdout.channel.recv_exit_status()
+        print(f"Installed pip on {instance_ip}")
+        stdin, stdout, stderr = client.exec_command("pip3 install sklearn")
+        stdout.channel.recv_exit_status()
+        print(f"Installed sklearn on {instance_ip}")
+        return
+
 
 def main():
     master = Master()
@@ -65,7 +92,8 @@ def main():
                 master.free_instance(msg[1])
                 break
             elif msg[0] == "get":
-                instance_id = master.get_instance().encode("utf-8")
-                conn.send(instance_id)
+                instance_ip = master.get_instance().encode("utf-8")
+                conn.send(instance_ip)
+
 
 main()
